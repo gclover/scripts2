d58fb43e1a91eda5622a3c4e017b2387c13ca6e0
gclover <clover.gch@gmail.com>
2013-04-06 23:11:25 +0800

    add fabfile.py project.clj

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+
+import os
+
+from fabric.api import *
+
+env.hosts = ['ip:port']
+env.user = 'user'
+env.password = 'password'
+
+def command():
+	pass
+
+
+
--- project.clj
+++ project.clj
@@ -1,11 +1,19 @@
-(defproject repotest "0.1.0-SNAPSHOT"
+(defproject jvm-tpl "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-		 [local/netty "3.2.5"]]
-  :repositories [["local" "http://10.200.41.5:4041/nexus/content/repositories/releases/"]
+  :dependencies [[org.clojure/clojure "1.4.0"]] 
+ ; :uberjar-exclusions [#"hadoop*"]
+  :aot [#"jvm-tpl*"]
+  :source-paths ["src/main/clojure"]
+  :test-paths ["src/test/clojure"]
+  :java-source-paths ["src/main/java"]
+  :repositories [["local" "http://10.200.41.5:4041/nexus/content/repositories/releases"]
 		 ["clojars" "http://clojars.org/repo"]
                  ["maven2" "http://repo1.maven.org/maven2"]]
-  :main repotest.core)
+  :offline? true
+  :plugins [[org.clojure/clojure-contrib "1.2.0"]]
+  :omit-source true
+  ;:main jvm-tpl.core
+)
